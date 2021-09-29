a764c0354fd225eaf88757529dabcf0c8c13f381
Johannes Hauschild <git@johannes-hauschild.de>
2021-09-29 19:02:22 +0200

    fixup setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 # this file allows to install this repository, locally with
 #     pip install -e .
 # or online with
-#     pip install https://github.com/jhauschild/WinterschoolBarcelona2021
+#     pip install git+git://github.com/jhauschild/WinterschoolBarcelona2021
 # to uninstall, simply
 #     pip uninstall tenpy-toycodes
-from setuptools import setup, find_packages
+
+from setuptools import setup
 
 setup(
     name='tenpy-toycodes',
     version='0.1.0',
-    packages=find_packages(include=['toycodes'])
+    packages=['toycodes'])
 )
